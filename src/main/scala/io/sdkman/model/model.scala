80dc739f38570f766f160608798e99a8bf9b8d78
Hector Geraldino <hgeraldino@gmail.com>
2021-11-04 21:47:28 -0400

    Add checksums field + enums

--- src/main/scala/io/sdkman/model/model.scala
+++ src/main/scala/io/sdkman/model/model.scala
@@ -1,5 +1,40 @@
 package io.sdkman.model
 
+sealed trait ChecksumAlgorithm {
+  def id: String
+  def priority: Integer
+}
+
+case object MD5 extends ChecksumAlgorithm {
+  override val id = "MD5"
+  override val priority = 0
+}
+
+case object SHA1 extends ChecksumAlgorithm {
+  override val id = "SHA-1"
+  override val priority = 1
+}
+
+case object SHA224 extends ChecksumAlgorithm {
+  override val id = "SHA-224"
+  override val priority = 2
+}
+
+case object SHA256 extends ChecksumAlgorithm {
+  override val id = "SHA-256"
+  override val priority = 3
+}
+
+case object SHA384 extends ChecksumAlgorithm {
+  override val id = "SHA-384"
+  override val priority = 4
+}
+
+case object SHA512 extends ChecksumAlgorithm {
+  override val id = "SHA-512"
+  override val priority = 5
+}
+
 case class Application(alive: String, stableCliVersion: String, betaCliVersion: String)
 
 case class Candidate(
@@ -17,5 +52,6 @@ case class Version(
     platform: String,
     url: String,
     vendor: Option[String] = None,
-    visible: Option[Boolean] = Some(true)
+    visible: Option[Boolean] = Some(true),
+    checksums: Option[Map[String, String]] = None
 )
